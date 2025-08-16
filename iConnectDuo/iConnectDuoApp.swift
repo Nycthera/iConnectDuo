77c4be8cede7c08a2879967183ff8a513aee8d29
Liu Weimi Chris <notexttospeachfmsp@gmail.com>
2025-08-16 10:31:07 +0800

    Initial Commit

---
+++ iConnectDuo/iConnectDuoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iConnectDuoApp.swift
+//  iConnectDuo
+//
+//  Created by Chris  on 16/8/25.
+//
+
+import SwiftUI
+
+@main
+struct iConnectDuoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
